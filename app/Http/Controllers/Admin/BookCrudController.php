ea0320d087204f1c245da4e92512c07dd95d72db
Cools1337 <cools1337@mail.ru>
2021-10-07 22:41:55 +0300

    Initial commit

---
+++ app/Http/Controllers/Admin/BookCrudController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\BookRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class BookCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class BookCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Book::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/book');
+        CRUD::setEntityNameStrings('book', 'books');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        //CRUD::setFromDb(); // columns
+        $this->crud->addColumns([
+        [
+        //'type'           => 'checkbox',
+        'name'=> 'name',
+        'label'=> 'Название'
+        ],
+        [
+        'name'=> 'price',
+        'label'=> 'Цена'
+        ],
+        [
+        'name'=> 'authors',
+        'label'=> 'Автор'
+        ]
+        ]);
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - CRUD::column('price')->type('number');
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(BookRequest::class);
+
+        //CRUD::setFromDb(); // fields
+        $this->crud->addFields([
+        [
+        'name' => 'name',
+        'type' => 'text',
+        'label' => "Название книги"
+        ],
+        [
+        'name' => 'price',
+        'type' => 'number',
+        'label' => "Цена"
+        ],
+        [
+        'name'     => "authors",
+        'type'      => 'select2_multiple',
+        'label'      => 'Автор',
+        'model'     => "App\Models\Author",
+        'attribute' => 'name',
+        'pivot'     => true,
+        'allows_null'     => false,
+        ]
+        ]);
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
